c39a0604ebe84725a0312b0945323a59086839ca
eekbk <eekbk@yahoo.com>
2022-04-12 21:04:24 +0000

    day 2 homework done

--- projects/scratch-pad/day-2/homework/fizz-buzz.js
+++ projects/scratch-pad/day-2/homework/fizz-buzz.js
@@ -12,6 +12,33 @@
 */
 function fizzBuzz() {
     // YOUR CODE GOES BELOW HERE //
+    // loop beginning at 1, ending at 100
+    for (var i = 1; i < 101; i++) {
+    // check if current number is multiple of 3 AND 5
+        if(i % 3 === 0 && i % 5 ===0) {
+            // if so, print 'FizzBuzz'
+            console.log('FizzBuzz');
+        // check if current number is multiple of 3
+        } else if(i % 3 === 0) {
+            // if so, print 'Fizz'
+            console.log('Fizz');
+        // check if current number is multiple of 5
+        } else if(i % 5 === 0) {
+            // if so, print 'Buzz'
+            console.log('Buzz');
+        // otherwise print the number
+        } else {
+            console.log(i);
+        }
+
+
+
+
+
+
+
+    }
+
 
 
 
