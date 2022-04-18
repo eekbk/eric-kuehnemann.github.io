d513572235ddca20cec90b71fbdd66aecca8ffa3
eekbk <eekbk@yahoo.com>
2022-04-18 03:33:04 +0000

    workin on variables Studies

--- Studies/variables.js
+++ Studies/variables.js
@@ -1,6 +1,13 @@
 /**
-VARIABLES:
-
-0. Variables are
-
+* VARIABLES:
+*
+* 0. To hold things in memory during the life-cycle of a program, we can use variables. 
+* Variables are named identifiers that can point to values of a particular type, like a
+* Number, a String, Boolean, Array, Objecto or another data-type. Variables are called so
+* because once created, we cam CHANGE the value (and type of value) to which they point.
+*
+* 1. To create a variable we use the keyword, var, followed by a name (id or alias) for 
+* our variable.
+*
+* 2. There are 2 phases of using variables: declaration and initialization (or assignment).
  */
