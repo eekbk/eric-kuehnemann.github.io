a8529d3f67be225bc9a592a6c7d2264154a92a24
eekbk <eekbk@yahoo.com>
2022-05-11 17:02:48 +0000

    studies finished

--- Studies/functions.js
+++ Studies/functions.js
@@ -38,7 +38,42 @@
  *          parameter was acting as a place-holder. You can call a function like so: */
             sum(3, 4); // here we passed in 3 and 4 as the arguments. 
 /**
- *          Of course, in the above example, the information had nothing to contain it, so it kind of
- *          gets 'lost'. You could however save the result to a variable. */
+ *          Of course, in the above example, the information had nothing to contain it, meaning it 
+ *          wasn't stored in memory, so it kind of gets 'lost'. You could however save the result to 
+ *          a variable. */
             let result = sum(3, 4);
             console.log(result); // prints 7
+/**
+ *  2. SCOPE
+ *      All variables declared within a function, are function scoped, regardless if the keyword used
+ *          is var, let or const. This means that if the variable cannot be accessed outside of the 
+ *          function. */
+            function greeting(){
+                var message = 'Hello there!';
+                console.log(message);
+            }
+
+            console.log(message); // results in a ReferenceError: message is not defined
+/**
+ *  3. CLOSURE
+ *      Closure is the ability of a function created inside another function (a 'parent' function) to
+ *          hold onto any variables declared within that function. You can think of the parent function
+ *          as the home the child function grew up in and even though the child moves out, and maybe the 
+ *          home no longer exists, the child has memory of the information it learned in its environment. */
+            function describeMomsHouse(street, city){
+                let color = 'green';
+                let tree = 'pine';
+                let car = true;
+                return function(){
+                    console.log(`I grew up on ${street} in ${city}. The house was ${color}, there was a ${tree} tree in the yard. Did we have a car? It is ${car}.`)
+                };
+            }
+            let myLifeStory = describeMomsHouse('Park Lane', 'North Syracuse');
+            myLifeStory();      // prints "I grew up on Park Lane in North Syracuse. The house was green,
+                                //      there was a pine tree in the yard. Did we have a car? It is true."
+/**
+ *          In the above example, the parent function, describeMomsHouse, returned a function. When we saved
+ *          the function invocation with arguments to a variable and then invoked THAT function, all of the 
+ *          variables and arguments from the original declaration and subsequent invocation were remembered
+ *          and used in the function.
+ */
